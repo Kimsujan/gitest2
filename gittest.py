0ed8d31b1bdca3b2df37c57d88b3c6a1ee599e19
Kimsujan <rlatjwls0122@naver.com>
2022-05-23 17:11:25 +0900

    2884

--- gittest.py
+++ gittest.py
@@ -1,1 +1,14 @@
-print('gittest_3')
+a, b = map(int, input().split())
+if b == 45:
+    b = 0
+elif b < 45:
+    if a == 0:
+        b = b + 60 - 45
+        a = 23
+    else:
+        b = b + 60 - 45
+        a = a - 1
+else:
+    b = b - 45
+
+print(a, b)
